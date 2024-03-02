0f2f041a179eacd8a0cce56fefd04e3b1b5ed835
Vinicius Lima <viniciuscosta90@gmail.com>
2024-03-02 09:19:05 -0300

    ...

--- estabelecimento.php
+++ estabelecimento.php
@@ -1,43 +1,35 @@
 <?php
-    require_once "ws-security.php";
+require_once "ws-security.php";
+$msg = array();
+if(!isset($_GET['cnes'])) {
+	$msg["code"] = 400;
+	$msg["msg"] = "Parâmetro 'cnes' não encontrado";	
+} else {
+	try {
+		$options = array( 'location' => 'https://servicoshm.saude.gov.br/cnes/EstabelecimentoSaudeService/v1r0', 
+						  'encoding' => 'utf-8', 
+						  'soap_version' => SOAP_1_2,
+						  'connection_timeout' => 180,
+						  'trace'        => 1, 
+						  'exceptions'   => 1 );
+		$client = new SoapClient('https://servicoshm.saude.gov.br/cnes/EstabelecimentoSaudeService/v1r0?wsdl', $options);   
+		$client->__setSoapHeaders(soapClientWSSecurityHeader('CNES.PUBLICO', 'cnes#2015public'));
+		$arguments = array( 'est' => array(
+								'FiltroPesquisaEstabelecimentoSaude' => array(
+										'CodigoCNES' => array(
+										'codigo'      => $_GET['cnes']
+									)
+								)
+							)
+						);
+		$result = $client->__soapCall('consultarEstabelecimentoSaude', $arguments);		
+		$msg["code"] = 200;
+		$msg["msg"] = "";
+		$msg["data"] = $result;
+	} catch(Exception $e) {
+		$msg["code"] = 503;
+		$msg["msg"] = trim($e->faultstring);
+	}
+}
+echo json_encode($msg);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Estabelecimento de Saúde</title>
-</head>
-<body>
-
-<?php
-
-$options = array( 'location' => 'https://servicos.saude.gov.br/cnes/EstabelecimentoSaudeService/v1r0', 
-                  'encoding' => 'utf-8', 
-                  'soap_version' => SOAP_1_2,
-                  'connection_timeout' => 180,
-                  'trace'        => 1, 
-                  'exceptions'   => 1 );
-
-$client = new SoapClient('https://servicos.saude.gov.br/cnes/EstabelecimentoSaudeService/v1r0?wsdl', $options);   
-$client->__setSoapHeaders(soapClientWSSecurityHeader('CNES.PUBLICO', 'cnes#2015public'));
-
-$function = 'consultarEstabelecimentoSaude';
-
-$arguments = array( 'est' => array(
-						'FiltroPesquisaEstabelecimentoSaude' => array(
-								'CodigoCNES' => array(
-	                            'codigo'      => '0010456'
-	                        )
-						)
-                    )
-                );
-
-$result = $client->__soapCall($function, $arguments);
-
-print("<pre>".print_r($result,true)."</pre>");
-
-?>
-	
-</body>
-</html>
--- pesquisa-profissional.php
+++ pesquisa-profissional.php
@@ -1,42 +1,35 @@
 <?php
-	include "ws-security.php";
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Dados Profissional</title>
-</head>
-<body>
-
-<?php
-
-	$options = array( 'location' => 'https://servicoshm.saude.gov.br/cnes/ProfissionalSaudeService/v1r0', 
-	'encoding' => 'utf-8', 
-	'soap_version' => SOAP_1_2,
-	'connection_timeout' => 180,
-	'trace'        => 1, 
-	'exceptions'   => 1 );
-
-	$client = new SoapClient('https://servicoshm.saude.gov.br/cnes/ProfissionalSaudeService/v1r0?wsdl', $options);   
-	$client->__setSoapHeaders(soapClientWSSecurityHeader('CNES.PUBLICO', 'cnes#2015public'));
-
-	$function = 'consultarProfissionalSaude';
-
-	$arguments= array( 'prof' => array(
-							'FiltroPesquisaProfissionalSaude' => array(
-								'CPF' => array(
-	                            	'numeroCPF'  => '74558331304'
-	                        	)
+require_once "ws-security.php";
+$msg = array();
+if(!isset($_GET['cpf'])) {
+	$msg["code"] = 400;
+	$msg["msg"] = "Parâmetro 'cpf' não encontrado";	
+} else {
+	try {
+		$options = array( 'location' => 'https://servicoshm.saude.gov.br/cnes/ProfissionalSaudeService/v1r0', 
+						'encoding' => 'utf-8', 
+						'soap_version' => SOAP_1_2,
+						'connection_timeout' => 180,
+						'trace'        => 1, 
+						'exceptions'   => 1 );
+		$client = new SoapClient('https://servicoshm.saude.gov.br/cnes/ProfissionalSaudeService/v1r0?wsdl', $options);   
+		$client->__setSoapHeaders(soapClientWSSecurityHeader('CNES.PUBLICO', 'cnes#2015public'));
+		$arguments= array( 'prof' => array(
+								'FiltroPesquisaProfissionalSaude' => array(
+									'CPF' => array(
+										'numeroCPF'  => $_GET['cpf']
+									)
+								)
 							)
-	                    )
-	                );
-
-	$result = $client->__soapCall($function, $arguments);
-
-	print("<pre>".print_r($result,true)."</pre>");
+						);
+		$result = $client->__soapCall('consultarProfissionalSaude', $arguments);
+		$msg["code"] = 200;
+		$msg["msg"] = "";
+		$msg["data"] = $result;
+	} catch(Exception $e) {
+		$msg["code"] = 503;
+		$msg["msg"] = trim($e->faultstring);
+	}
+}
+echo json_encode($msg);
 ?>
-	
-</body>
-</html>
